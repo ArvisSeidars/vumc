45b6ff5e83df9247f68227e47b39e15cfb5de25f
ArvisSeidars <boomtix@inbox.lv>
2022-09-26 22:02:25 +0300

    update 1

--- src/main/java/lekcijaViens/majasDarbs/Main.java
+++ src/main/java/lekcijaViens/majasDarbs/Main.java
@@ -1,4 +1,93 @@
 package lekcijaViens.majasDarbs;
 
 public class Main {
+
+    String Valsts = "Lietuva";
+    String teksts = "Suņa vārds ir: ";
+        System.out.println(teksts + sunaVards);
+
+    byte sunaVecums = 7;
+    String teksts1 = "Suņa vecums ir: ";
+        System.out.println(teksts1 + sunaVecums);
+    //byte pieņem veselu skaitli no -127 līdz +127
+
+    double grauduSvars = 150.23;
+        System.out.println( "Nopirkti : " + grauduSvars );
+
+    // boolean nosaka ir patiess vai nepatiess
+    boolean isOpen = true;
+    //Šodien ir sestdiena, lielveikals ir atvērts
+    boolean isSuperMarketOpen = true;
+    //Turpretīm skolas ir slēgtas
+    boolean isSchoolOpen = false;
+
+        System.out.println("Vai veikals ir atvērts?" + isSuperMarketOpen);
+
+    boolean vaiSunsIrPotets = true;
+
+    char valuta = '$';
+    char dzimums = 'v';
+    char dzimums2 = 's';
+
+    int a = 5;
+    int b = 10;
+    int summa;
+    summa = a + b;
+
+
+        System.out.println(summa);
+
+    summa = a + a;
+
+        System.out.println(summa);
+
+    String name = "Juris";
+        System.out.println(name);
+    name = "Jānis";
+        System.out.println(name);
+
+    int x = 10;
+    int y = -45;
+    int rezultats = x+y;
+        System.out.println(rezultats);
+
+    rezultats = 10/2;
+        System.out.println(rezultats);
+
+    rezultats = y-x;
+        System.out.println(rezultats);
+
+    rezultats = 11%2;
+        System.out.println(rezultats);
+
+    int x = 5;
+    int y = -45;
+
+    int rezultats = x + y;
+        System.out.println(rezultats);
+
+    rezultats = x - y;
+        System.out.println(rezultats);
+
+    rezultats = x * y;
+        System.out.println(rezultats);
+
+    rezultats = 10 / 2;
+        System.out.println(rezultats);
+    rezultats = 13 % 2;
+        System.out.println(rezultats);
+
+    rezultats = 5*(3+7)/2;
+        System.out.println(rezultats);
+
+    rezultats = 5*(3+7)/4;
+        System.out.println(rezultats);
+
+
+
+
+
+
+
+
 }
