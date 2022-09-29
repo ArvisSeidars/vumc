33fb3358c5e54617c1cf122618660fec95294e18
ArvisSeidars <boomtix@inbox.lv>
2022-09-29 14:13:08 +0300

    Create main1.java
    MajasDarbs

---
+++ src/main/java/lekcijaViens/majasDarbs/main1.java
@@ -0,0 +1,55 @@
+package lekcijaViens.majasDarbs;
+
+public class main1 {
+    public static void main(String[] args) {
+        String valsts = "Igaunijas Republika ";
+        System.out.println("Valsts : " + valsts);
+        String iedzDaudz = "Iedzīvotāji : ";
+        int skaits = 1329460;
+        System.out.println(iedzDaudz + skaits );
+        String plat = "Platība : ";
+        int km = 45339;
+        String kkmm = "km²";
+        System.out.println(plat + km  +  kkmm );
+        String galvaspilseta = "Galvaspilsēta : ";
+        String pils = "Tallina";
+        System.out.println(galvaspilseta + pils);
+        String esIr = "Eiropas Savienība : ";
+        String ja = "JĀ.";
+        System.out.println(esIr + ja);
+        String valuta = "Valsts valūta : ";
+        char vl = '€';
+        System.out.println(valuta + vl);
+        String apr = "ir valsts Ziemeļeiropā, viena no trim Baltijas valstīm. ";
+        String robez = "Tā robežojas ar Krieviju austrumos un Latviju dienvidos, tai ir jūras robeža ar Somiju, Zviedriju un Latviju.";
+        String apr3 = "Igaunija atrodas jaukto mežu dabas zonā. Gandrīz pusi Igaunijas teritorijas klāj meži, kuros aug aptuveni 90 koku un krūmu sugu. Visizplatītākās sugas ir priedes, egles, bērzi un apses. Mazāk aug ozoli, kļavas, gobas un oši. Otru valsts pusi aptuveni vienādās daļās veido pļavas un purvi, kuros ir sastopamas aptuveni 1500 augu sugas.";
+        System.out.println(valsts + apr + robez + apr3 );
+
+
+
+        int a = 948;
+        int b = 237;
+        int c = 2;
+        int rezultats;
+        rezultats = a + b;
+        System.out.println(rezultats);
+        rezultats = a * b;
+        System.out.println(rezultats);
+        rezultats = a / b;
+        System.out.println(rezultats);
+        rezultats = a * (a + b) / c;
+        System.out.println(rezultats);
+        rezultats = 4%3;
+        System.out.println(rezultats);
+
+
+
+
+
+
+
+
+
+
+    }
+ }
